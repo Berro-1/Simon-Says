2627f88b2f1eba1c79105c33f0db945a9128bc43
Berro-1 <hussienberro1@gmail.com>
2024-06-30 18:07:31 +0300

    added function to generate the next color in game and added css fade effect

--- game.js
+++ game.js
@@ -1,15 +1,30 @@
-const buttonColors = ["red", "blue", "green", "yellow"]
+let buttonColours = ["red", "blue", "green", "yellow"];
+
 let gamePattern = [];
-let userClickedPattern = []
-let started = false
-let level = 0
+let userClickedPattern = [];
 
-document.addEventListener('keypress', () => {
+let started = false;
+let level = 0;
 
-    if (!started) {
-        document.querySelector("#level-title").textContent = `Level ${level}`
-        nextSequence();
-        started = true
-    }
-})
+document.addEventListener("keypress", function () {
+  if (!started) {
+    document.getElementById("level-title").textContent = "Level " + level;
+    nextSequence();
+    started = true;
+  }
+});
 
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    document.getElementById("level-title").textContent = "Level " + level;
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+  
+    const button = document.getElementById(randomChosenColour);
+    button.classList.add("fade");
+    setTimeout(() => button.classList.remove("fade"), 200);
+    playSound(randomChosenColour);
+  }
+  
